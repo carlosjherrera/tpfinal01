b535f22fc3d2c9dcfeec5f7547ab31cd3e7370fc
Carlos Herrera <studio3dcad@gmail.com>
2021-12-12 20:41:28 -0300

    rev E 12 12 Func Login y sessiones

--- app.js
+++ app.js
@@ -5,14 +5,16 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 require('dotenv').config();
+var session = require('express-session');
 
 var indexRouter = require('./routes/index');//<<<<<<<<<<<<<<<<<<
 //var usersRouter = require('./routes/users');
-var proyectosRouter = require ('./routes/proyectos');
-var publicacionesRouter = require ('./routes/publicaciones');
-var novedadesRouter = require ('./routes/novedades');
-var contactoRouter = require ('./routes/contacto');
+var proyectosRouter = require('./routes/proyectos');
+var publicacionesRouter = require('./routes/publicaciones');
+var novedadesRouter = require('./routes/novedades');
+var contactoRouter = require('./routes/contacto');
 var loginRouter = require('./routes/admin/login');
+var adminNovedadesRouter = require('./routes/admin/novedades');
 
 var app = express();
 
@@ -26,28 +28,50 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(session({
+  secret: '12345678911234567890',
+  resave: false,
+  saveUninitialized: true,
+  cookie: {maxAge: 70000}
+}))
+
+secured = async function (req, res, next) {
+  try {
+    console.log(req.session.id_usuario)
+
+      if (req.session.id_usuario) {
+        next()
+      } else {
+        res.redirect('/admin/login')
+      }
+    } catch (error) {
+      console.log(error)
+    }
+  } //cierra secured
+
 app.use('/', indexRouter);
-//app.use('/users', usersRouter); //<<<<<<<<<<<<<<<<<<
-app.use('/proyectos', proyectosRouter);
-app.use('/publicaciones', publicacionesRouter);
-app.use('/novedades', novedadesRouter);
-app.use('/contacto', contactoRouter);
-app.use('/admin/login', loginRouter);
+  //app.use('/users', usersRouter); //<<<<<<<<<<<<<<<<<<
+  app.use('/proyectos', proyectosRouter);
+  app.use('/publicaciones', publicacionesRouter);
+  app.use('/novedades', novedadesRouter);
+  app.use('/contacto', contactoRouter);
+  app.use('/admin/login', loginRouter);
+  app.use('/admin/novedades',secured, adminNovedadesRouter);
+ 
+  // catch 404 and forward to error handler
+  app.use(function (req, res, next) {
+    next(createError(404));
+  });
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
+  // error handler
+  app.use(function (err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
+  });
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
-
-module.exports = app;
+  module.exports = app;
--- routes/admin/login.js
+++ routes/admin/login.js
@@ -1,10 +1,39 @@
 var express = require('express');
+const session = require('express-session');
 var router = express.Router();
+var usuariosModel = require('./../../models/usuariosModels');
 
 router.get('/', function (req, res, next) {
   res.render('admin/login', {
     layout: 'admin/layout'
   }) // admin.hbs
 });
+
+router.post('/', async function (req, res, next) {
+  try {
+
+    var usuario = req.body.usuario;
+    var password = req.body.password;
+
+    var data = await usuariosModel.getUserAndPassword(usuario,password);
+
+    if (data != undefined) {
+      req.session.id_usuario = data.id; // 1
+      req.session.nombre = data.usuario; // Carlos
+
+      res.redirect('/admin/novedades');
+    } else {
+      res.render('admin/login', {
+        layout: 'admin/layout',
+        error: true
+
+      })
+    }
+
+  } catch (error) {
+    console.log(error);
+  }
+});
+
 
 module.exports = router;
