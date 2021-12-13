c236e4e64513f7e533d195ae3862626ff9ea4950
Carlos Herrera <studio3dcad@gmail.com>
2021-12-13 09:50:40 -0300

    rev G 13 12 acciones agregar y borrar novedades

--- routes/admin/novedades.js
+++ routes/admin/novedades.js
@@ -13,6 +13,45 @@ var novedades = await novedadesModel.getNovedades();
     });
 });
 
+router.get('/agregar', function(req,res,next){
+    res.render('admin/agregar',{
+        layout:'admin/layout'
+    });
+   
+});/*--- */
 
+router.post('/agregar', async (req, res, next) => {
+    try {
+        console.log(req.body);
+
+        if(req.body.titulo != "" && req.body.subtitulo != "" && req.body.cuerpo != ""){
+            await novedadesModel.insertNovedad(req.body);
+            res.redirect('/admin/novedades');
+        }else{
+            res.render('admin/agregar',{
+                layout:'admin/layout',
+                error:true,
+                message: 'Todos los campos son requeridos'
+            })
+        }
+
+    } catch (error) {
+        console.log(error);
+        res.render('admin/agregar', {
+            layout: 'admin/layout',
+            error: true,
+            message: 'No se cargo la novedad'
+        })
+    }
+})
+
+/*eliminar*/
+
+router.get('/eliminar/:id',async(req,res,next)=>{
+    var id = req.params.id; //id 
+
+    await novedadesModel.deleteNovedadByID(id);
+    res.redirect('/admin/novedades');
+}); 
 
 module.exports = router;
